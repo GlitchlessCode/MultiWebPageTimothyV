df5beb6d5ee92fc2e0103ac66beb3e68eb2723e4
TurtleY0da <timothy.veugelers@gmail.com>
2021-09-17 22:28:19 -0600

    Added all Functionality to Buttons

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,1 +1,39 @@
 // Button Management Script by Timothy V
+console.log("start");
+// -- Initialize Variables --
+
+// HTML Elements
+let homeBtn = document.getElementById("homeBtn");
+let aboutBtn = document.getElementById("aboutBtn");
+let quizBtn = document.getElementById("quizBtn");
+
+// Glbl Variables
+// (None)
+
+
+// -- Add Event Listeners --
+if(homeBtn != null){
+    homeBtn.addEventListener("click", gotoHome);
+}
+if(aboutBtn != null){
+    aboutBtn.addEventListener("click", gotoAbout);
+}
+if(quizBtn != null){
+    quizBtn.addEventListener("click", gotoQuiz);
+}
+
+// -- Functions --
+
+// Event Functions
+function gotoHome() {
+    location.replace("/index.html");
+}
+
+function gotoAbout() {
+    location.replace("/about.html");
+    console.log("About");
+}
+
+function gotoQuiz() {
+    location.replace("/quiz.html");
+}
