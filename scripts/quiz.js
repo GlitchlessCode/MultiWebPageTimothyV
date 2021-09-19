d487c39e7b22cdb22d1c3ba4cf11659102148fa5
TurtleY0da <timothy.veugelers@gmail.com>
2021-09-19 00:20:31 -0600

    Adding HTML to quiz page

---
+++ scripts/quiz.js
@@ -0,0 +1,15 @@
+// Quiz Management Script by Timothy V
+
+// -- Initialize Variables --
+
+// HTML Elements
+
+// Glbl Variables
+
+
+// -- Add Event Listeners --
+
+
+// -- Functions --
+
+// Event Functions
